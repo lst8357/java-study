c5e8aed3a9d98978096cae2d40280aa709a53f04
lst <lst1027@naver.com>
2021-11-18 15:58:54 +0900

    11월18일 오후

---
+++ CH04_Loop/src/application/IfElseIfElseExam.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Scanner;
+
+public class IfElseIfElseExam {
+
+	public static void main(String[] args) {
+		// 스캐너
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("메뉴");
+		System.out.println("====");
+		System.out.println();
+		System.out.println("1. 프린트 '헬로우' ");		
+		System.out.println("2. 프린트 '안녕?' ");
+		System.out.println("3. 프로그램 종료 ");
+		System.out.println();
+		System.out.println("옵션을 선택 > ");
+		int input = scanner.nextInt();
+		scanner.close(); // 스캐너 종료
+		
+		if (input == 1) {
+			System.out.println("헬로우");
+		}
+		else if(input == 2) {
+			System.out.println("안녕 ?");
+			
+		}
+		else if(input == 3) {
+			System.out.println("프로그램 종료");
+			
+		}
+		else if(input == 10000) {
+			System.out.println("이스터에그를 발견했습니다.");
+		}
+		else { // 위의 조건들이 아닐경우 실행
+			System.out.println("잘못된 옵션입니다.");
+		}
+		
+		
+		System.out.println("종료합니다...");
+
+	}
+
+}
