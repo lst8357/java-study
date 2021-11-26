9bfdd49905f0b28de536395788f845d65f3f650c
lst <lst1027@naver.com>
2021-11-26 16:46:49 +0900

    11월26일 오후

--- CH11_GameEx/src/game/objects/GameObject.java
+++ CH11_GameEx/src/game/objects/GameObject.java
@@ -3,6 +3,15 @@ package game.objects;
 public class GameObject {
 	private int id;
 	private String name;
+	private int[][] comparison = {
+							{ 0,	 -1,	 1},
+							{ 1,	  0,	-1},
+							{-1,	  1,	 0},
+	};
+	//상대의 (가위,바위,보)와 비교해서 결과 리턴
+	public int compareTo(GameObject ob) {
+		return comparison[id][ob.id];
+	}
 	
 	protected int getId() { //아이디 수정은 상속한 클래스에서만 사용
 		return id;
